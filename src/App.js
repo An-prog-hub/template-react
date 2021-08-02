4ce374e5290c467d811aaf853481c247437a66d7
Ankush Chauhan <ac.ankushchauhan.2006@gmail.com>
2021-08-02 16:51:20 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import MailOutlineIcon from "@material-ui/icons/MailOutline";
+import Image from "./images/myphoto.jpg";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+      <img src={Image} alt="myphoto" />
+
+      <div className="headerText">
+        <h1>
+          Hi ⚡, I'm {""}
+          <a href="https://linkedin.com/in/ankushchauhan14" target="_blank">
+            Ankush Chauhan
+          </a>
+        </h1>
+      </div>
+      <div className="text_body">
+        Hey 👋 I'm Ankush, a freelancer, developer, writer with
+        interests in entrepreneurship and decentralise finance. <br />
+        Developing new ways to make human life more accessible and friendly on
+        the planet :)
+      </div>
+      <Button
+        className="buttons"
+        variant="contained"
+        href="/about"
+        color="primary"
+      >
+        About Me <ArrowForwardIosIcon />
+      </Button>
+      <Button
+        className="buttons"
+        variant="contained"
+        href="/projects"
+        color="primary"
+      >
+        Projects
+        <ArrowForwardIosIcon />
+      </Button>
+      <Button
+        className="buttons"
+        variant="contained"
+        href="/notes"
+        color="primary"
+      >
+        Notes <ArrowForwardIosIcon />
+      </Button>
+
+      <div className="socials-links">
         <a
-          className="App-link"
-          href="https://reactjs.org"
+          className="social-link"
+          href="https://linkedin.com/in/ankushchauhan14"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          <LinkedInIcon style={{ color: "orange" }} fontSize="large" />
         </a>
-      </header>
+        <a
+          className="social-link"
+          href="https://twitter.com/anproghub"
+          target="_blank"
+        >
+          <TwitterIcon style={{ color: "orange" }} fontSize="large" />
+        </a>
+        <a
+          className="social-link"
+          href="https://github.com/An-prog-hub"
+          target="_blank"
+        >
+          <GitHubIcon style={{ color: "orange" }} fontSize="large" />
+        </a>
+        <a
+          className="social-link"
+          href="mailto:ac.ankushchauhan.2006@gmail.com"
+          target="_blank"
+        >
+          <MailOutlineIcon style={{ color: "orange" }} fontSize="large" />
+        </a>
+      </div>
     </div>
   );
 }
